cccf1de5e5f340b46b29c0f7005dab11c106b31c
Bakyt <djumabaevb@gmail.com>
2021-04-27 13:10:43 +0600

    android kotlin extensions added to gradle, so that we could use: id without instantiating objects of views and buttons

--- app/src/main/java/com/example/ageapplication/MainActivity.kt
+++ app/src/main/java/com/example/ageapplication/MainActivity.kt
@@ -2,10 +2,17 @@ package com.example.ageapplication
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Toast
+import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        btnDateClicker.setOnClickListener { Toast.makeText(this, "Button works", Toast.LENGTH_LONG).show() }
+
+
     }
 }
